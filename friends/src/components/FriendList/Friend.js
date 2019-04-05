76bb934919715c4cfa07b6455030581d3bb8dbdb
Zero <jrr043@latech.edu>
2019-04-04 22:02:29 -0500

    MVP

--- friends/src/components/FriendList/Friend.js
+++ friends/src/components/FriendList/Friend.js
@@ -1,14 +1,81 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Friend.css';
 
-export default function Friend({friend}) {
-    console.log(friend);
-    return (
-        <div className="friend-wrapper">
-            <p>{friend.id}</p>
-            <p>{friend.name}</p>
-            <p>{friend.age}</p>
-            <p>{friend.email}</p>
-        </div>
-    );
+class Friend extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            edit : false,
+            newFriend : {
+                id : this.props.friend.id,
+                name : '',
+                age : '',
+                email : ''
+            }
+        }
+    }
+
+    inputChange = e => {
+        e.persist();
+        let value = e.target.value;
+        this.setState(prevState => ({
+        newFriend: { ...prevState.newFriend, [e.target.name]: value }
+        }));
+    }
+
+    render() {
+        let body;
+        if(!this.state.edit) { body =
+            <> 
+                <i className="far fa-edit" onClick={() => { this.setState(prevState =>{ return { edit:!prevState.edit}})}}> </i>
+                <i className="fas fa-trash-alt" onClick={(e) => {this.props.buttonHelper(e, this.props.friend)}}></i>
+                <p>{this.props.friend.id}</p>
+                <p>{this.props.friend.name}</p>
+                <p>{this.props.friend.age}</p>
+                <p>{this.props.friend.email}</p>
+            </>
+        }
+        if(this.state.edit) { body = 
+            <form
+                className="edit-form"
+                onSubmit={(e) => {
+                    setTimeout(this.setState({edit:false}), 5000)
+                    this.props.buttonHelper(e, this.state.newFriend)
+                }}
+            >   
+                <input 
+                        className="edit-input" 
+                        placeholder="Name" 
+                        value={this.state.name}
+                        name='name'
+                        type='text' 
+                        onChange={this.inputChange}
+                />
+                <input 
+                        className="edit-input" 
+                        placeholder="Age" 
+                        value={this.state.age}
+                        name='age'
+                        type='text' 
+                        onChange={this.inputChange}
+                />
+                <input 
+                        className="edit-input" 
+                        placeholder="Email" 
+                        value={this.state.email}
+                        name='email'
+                        type='text' 
+                        onChange={this.inputChange}
+                />
+                <button>Submit</button>
+            </form>
+        }
+        return (
+            <div className="friend-wrapper">
+                {body}
+            </div>
+        );
+    }
 }
+
+export default Friend;
--- friends/src/components/FriendList/FriendList.js
+++ friends/src/components/FriendList/FriendList.js
@@ -22,11 +22,23 @@ class FriendList extends Component {
         axios.post('http://localhost:5000/friends', newUser).then((res)=>this.setState({data:res.data}))
     }
 
+    buttonHelper = (e, friend) => {
+        e.preventDefault();
+        if (e.target.className.includes('trash')) {
+            console.log('delete');
+            axios.delete(`http://localhost:5000/friends/${friend.id}`).then(response => this.setState({data:response.data})).catch(err => console.log(err))
+        }
+        if (e.target.className.includes('edit')) {
+            console.log('edit');
+            axios.put(`http://localhost:5000/friends/${friend.id}`, friend).then(response => this.setState({data:response.data})).catch(err => console.log(err))
+        }
+    }
+
     render() {
         return (
             <>
-            <FriendForm sendData={this.sendData}/>
-            { this.state.data.map(item => <Friend friend={item}/>) }
+            <FriendForm sendData={this.sendData} type='create'/>
+            { this.state.data.map(item => <Friend friend={item} buttonHelper={this.buttonHelper}/>) }
             </>
         );
     }
